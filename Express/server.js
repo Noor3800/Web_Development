81b6ea045ae71a419e2299ac8785966e073e794d
Noor3800 <noorfatimaa3800@gmail.com>
2025-06-17 21:11:41 +0500

    added products

--- Express/server.js
+++ Express/server.js
@@ -1,54 +1,37 @@
-let express = require("express");
-let mongoose = require("mongoose");
-let expressLayouts = require("express-ejs-layouts");
-let server = express()
+const mongoose = require('mongoose');
+const Product = require('./models/Products');
 
+mongoose.connect('mongodb://127.0.0.1:27017/order_data')
+  .then(async () => {
+    await Product.insertMany([
+      {
+        title: 'dress 1',
+        price: 500,
+        description: 'Comfortable cotton T-shirt',
+        image: '/images/tshirt1.jpg'
+      },
+      {
+        title: 'dress 2',
+        price: 2000,
+        description: 'cotton dress',
+        image: '/images/men6.jpg'
+      }
+      ,
+      {
+        title: 'dress 3',
+        price: 2000,
+        description: 'comfortable dress',
+        image: '/images/men3.jpg'
+      },
+      {
+        title: 'dress 4',
+        price: 2000,
+        description: 'comfortable dress',
+        image: '/images/tshirt4.jpg'
+      }
+    ]);
+    console.log("Products added");
+    process.exit();
+  });
 
-server.set("view engine", "ejs");
-server.use(expressLayouts);
-server.use(express.static("public"));
-server.use(express.urlencoded());
-
-
-server.get("/index.html", async (req, res) => {
-  let Product = require("./models/product.model");
-  let products = await Product.find();
-  res.render("index", {products});
-});
-
-// server.get("/", (req, res) => {
-//   res.render("index");
-// });
-
-server.get("/indexcv.html", (req, res) => {
-  res.render("indexcv", {layout: false});
-});
-
-
-server.get("/login.html", (req, res) => {
-  return res.status(404).send("File Not Found");
-  res.render("homepage");
-});
-server.use("/", require("./controllers/admin/admin.products.controller"));
-
-server.get("/categories", (req, res) => {
-  res.render("categories");
-});
-server.get("/", async (req, res) => {
-  //   res.send("Hello AI Classs");
-  let Product = require("./models/product.model");
-  let products = await Product.find();
-  // return res.send(products);
-  res.render("homepage", { products });
-});
-
-
-mongoose.connect("mongodb://localhost:27017/DB").then(() => {
-  console.log("connected to db");
-});
-
-server.listen(4000, () => {
-  console.log("Server Started at localhost:4000");
-});
-
-
+  
