09af4d7450d418cb282e805ef987b5fab9399336
Noor3800 <noorfatimaa3800@gmail.com>
2025-06-17 14:58:47 +0500

    changed register.ejs

--- Express/server.js
+++ Express/server.js
@@ -7,6 +7,7 @@ let server = express()
 server.set("view engine", "ejs");
 server.use(expressLayouts);
 server.use(express.static("public"));
+server.use(express.urlencoded());
 
 
 server.get("/index.html", async (req, res) => {
@@ -22,6 +23,25 @@ server.get("/index.html", async (req, res) => {
 server.get("/indexcv.html", (req, res) => {
   res.render("indexcv", {layout: false});
 });
+
+
+server.get("/login.html", (req, res) => {
+  return res.status(404).send("File Not Found");
+  res.render("homepage");
+});
+server.use("/", require("./controllers/admin/admin.products.controller"));
+
+server.get("/categories", (req, res) => {
+  res.render("categories");
+});
+server.get("/", async (req, res) => {
+  //   res.send("Hello AI Classs");
+  let Product = require("./models/product.model");
+  let products = await Product.find();
+  // return res.send(products);
+  res.render("homepage", { products });
+});
+
 
 mongoose.connect("mongodb://localhost:27017/DB").then(() => {
   console.log("connected to db");
